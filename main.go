75d56cc728549977080fbfb9d623ebe1c72e9e8b
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-04-21 11:53:39 +0300

    chore: initialize project, implement structs

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Movie struct {
+  ID string `json:"id"`
+  Isbn string `json:"isbn"`
+  Title string `json:"title"`
+  Director *Director `json:"director"`
+}
+
+type Director struct {
+  FirstName string `json:"first_name"`
+  LastName string `json:"last_name"`
+}
+
+var movies []Movie
+
+func main() {
+  fmt.Println("Hello, World!")
+}
